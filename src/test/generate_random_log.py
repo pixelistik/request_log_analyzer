32e52628307e7865063fb9b3cb7229ec9411068c
Pixelistik <code@pixelistik.de>
2016-11-27 17:54:25 +0100

    Generate broken test log lines

--- src/test/generate_random_log.py
+++ src/test/generate_random_log.py
@@ -8,7 +8,22 @@ Example:
 will generate 1000 request lines and up to 1000 matching response lines.
 """
 import sys
-from random import randint
+from random import randint, choice
+
+
+def mutate(text, mutation_rate):
+    """
+    For 1 out of <mutation_rate> times, insert a random character into a line
+    """
+    if mutation_rate is None or not randint(0, mutation_rate) == 0:
+        return text
+
+    chars = list(text)
+    new_char = choice([" ", "X"])
+    position = randint(0, len(text) - 1)
+    chars[position] = new_char
+
+    return "".join(chars)
 
 try:
     count = int(sys.argv[1])
@@ -22,7 +37,11 @@ for i in range(0, count):
     id = randint(1, 99999)
     duration = randint(0, 20000)
     no_response_failure_rate = randint(0, 99)
+    mutation_rate = None # Set to e.g. 100 to damage every 100th line
 
-    print("08/Apr/2016:%d:%d:47 +0200 [%d] -> GET /content/%s/page.html HTTP/1.1" % (hour, minute, id, "some"))
+    line = "08/Apr/2016:%d:%d:47 +0200 [%d] -> GET /content/%s/page.html HTTP/1.1" % (hour, minute, id, "some")
+    print mutate(line, mutation_rate)
+
     if not no_response_failure_rate == 0:
-        print("08/Apr/2016:%d:%s:48 +0200 [%d] <- 200 text/html %dms" % (hour, minute, id, duration))
+        line = "08/Apr/2016:%d:%s:48 +0200 [%d] <- 200 text/html %dms" % (hour, minute, id, duration)
+        print mutate(line, mutation_rate)
