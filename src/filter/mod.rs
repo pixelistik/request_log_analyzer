44e22eb839f8e13c988ca38a5f7255b3204ccab2
Pixelistik <code@pixelistik.de>
2016-12-27 00:52:17 +0100

    Filtering a single pair is enough
    There is no full vec of all Request/Response pairs anymore.

--- src/filter/mod.rs
+++ src/filter/mod.rs
@@ -7,7 +7,7 @@ pub struct FilterConditions {
     pub latest_time: Option<Duration>,
 }
 
-fn matches_filter(pair: &RequestResponsePair, conditions: &FilterConditions) -> bool {
+pub fn matches_filter(pair: &RequestResponsePair, conditions: &FilterConditions) -> bool {
     let matches_include_terms: bool = match conditions.include_terms {
         Some(ref include_terms) => {
             include_terms.iter()
@@ -49,14 +49,6 @@ fn matches_filter(pair: &RequestResponsePair, conditions: &FilterConditions) -> 
     matches_include_terms && matches_exclude_terms && matches_time
 }
 
-pub fn filter(pairs: Vec<RequestResponsePair>, conditions: FilterConditions) -> Vec<RequestResponsePair> {
-    let filtered_pairs: Vec<RequestResponsePair> = pairs.into_iter()
-        .filter(|pair| matches_filter(&pair, &conditions))
-        .collect();
-
-    filtered_pairs
-}
-
 #[cfg(test)]
 mod tests {
     use log_parser::log_events::*;
@@ -64,26 +56,16 @@ mod tests {
     use chrono::*;
     use super::*;
 
-    fn get_fixture() -> Vec<RequestResponsePair> {
-        vec![
-            RequestResponsePair {
-                request: Request::new_from_log_line(&"08/Apr/2016:09:57:47 +0200 [001] -> GET /content/some/page.html HTTP/1.1".to_string()).unwrap(),
-                response: Response::new_from_log_line(&"08/Apr/2016:09:57:47 +0200 [001] <- 200 text/html 1ms".to_string()).unwrap(),
-            },
-            RequestResponsePair {
-                request: Request::new_from_log_line(&"08/Apr/2016:09:58:47 +0200 [02] -> GET /content/some/other.html HTTP/1.1".to_string()).unwrap(),
-                response: Response::new_from_log_line(&"08/Apr/2016:09:58:47 +0200 [02] <- 200 text/html 10ms".to_string()).unwrap(),
-            },
-            RequestResponsePair {
-                request: Request::new_from_log_line(&"08/Apr/2016:10:58:47 +0200 [03] -> GET /content/some/third.html HTTP/1.1".to_string()).unwrap(),
-                response: Response::new_from_log_line(&"08/Apr/2016:10:58:47 +0200 [03] <- 200 text/html 100ms".to_string()).unwrap(),
-            },
-        ]
+    fn get_fixture() -> RequestResponsePair {
+        RequestResponsePair {
+            request: Request::new_from_log_line(&"08/Apr/2016:09:57:47 +0200 [001] -> GET /content/some/page.html HTTP/1.1".to_string()).unwrap(),
+            response: Response::new_from_log_line(&"08/Apr/2016:09:57:47 +0200 [001] <- 200 text/html 1ms".to_string()).unwrap(),
+        }
     }
 
     #[test]
     fn test_filter_none() {
-        let request_response_pairs = get_fixture();
+        let pair = get_fixture();
 
         let conditions = FilterConditions {
             include_terms: None,
@@ -91,66 +73,105 @@ mod tests {
             latest_time: None,
         };
 
-        let result: Vec<RequestResponsePair> = filter(request_response_pairs, conditions);
+        let result = matches_filter(&pair, &conditions);
 
-        assert_eq!(result.len(), 3);
+        assert_eq!(result, true);
     }
 
     #[test]
-    fn test_filter_include_exclude() {
-        let request_response_pairs = get_fixture();
+    fn test_filter_include_request() {
+        let pair = get_fixture();
 
         let conditions = FilterConditions {
-            include_terms: Some(vec!["text/html".to_string()]),
-            exclude_terms: Some(vec!["third.html".to_string()]),
+            include_terms: Some(vec!["page.html".to_string()]),
+            exclude_terms: None,
             latest_time: None,
         };
 
-        let result: Vec<RequestResponsePair> = filter(request_response_pairs, conditions);
+        let result = matches_filter(&pair, &conditions);
 
-        assert_eq!(result.len(), 2);
-        assert_eq!(result[0].request.id, 1);
-        assert_eq!(result[1].request.id, 2);
+        assert_eq!(result, true);
+    }
+
+    #[test]
+    fn test_filter_include_response() {
+        let pair = get_fixture();
+
+        let conditions = FilterConditions {
+            include_terms: Some(vec!["text/html".to_string()]),
+            exclude_terms: None,
+            latest_time: None,
+        };
+
+        let result = matches_filter(&pair, &conditions);
+
+        assert_eq!(result, true);
+    }
+
+    #[test]
+    fn test_filter_exclude_request() {
+        let pair = get_fixture();
+
+        let conditions = FilterConditions {
+            include_terms: None,
+            exclude_terms: Some(vec!["page.html".to_string()]),
+            latest_time: None,
+        };
+
+        let result = matches_filter(&pair, &conditions);
+
+        assert_eq!(result, false);
+    }
+
+    #[test]
+    fn test_filter_exclude_response() {
+        let pair = get_fixture();
+
+        let conditions = FilterConditions {
+            include_terms: None,
+            exclude_terms: Some(vec!["text/html".to_string()]),
+            latest_time: None,
+        };
+
+        let result = matches_filter(&pair, &conditions);
+
+        assert_eq!(result, false);
     }
 
     #[test]
     fn test_filter_include_multiple() {
-        let request_response_pairs = get_fixture();
+        let pair = get_fixture();
 
         let conditions = FilterConditions {
-            include_terms: Some(vec!["page.html".to_string(), "third.html".to_string()]),
+            include_terms: Some(vec!["irrelevant.html".to_string(), "page.html".to_string()]),
             exclude_terms: None,
             latest_time: None,
         };
 
-        let result: Vec<RequestResponsePair> = filter(request_response_pairs, conditions);
+        let result = matches_filter(&pair, &conditions);
 
-        assert_eq!(result.len(), 2);
-        assert_eq!(result[0].request.id, 1);
-        assert_eq!(result[1].request.id, 3);
+        assert_eq!(result, true);
     }
 
     #[test]
     fn test_filter_exclude_multiple() {
-        let request_response_pairs = get_fixture();
+        let pair = get_fixture();
 
         let conditions = FilterConditions {
             include_terms: None,
-            exclude_terms: Some(vec!["page.html".to_string(), "third.html".to_string()]),
+            exclude_terms: Some(vec!["irrelevant.html".to_string(), "page.html".to_string()]),
             latest_time: None,
         };
 
-        let result: Vec<RequestResponsePair> = filter(request_response_pairs, conditions);
+        let result = matches_filter(&pair, &conditions);
 
-        assert_eq!(result.len(), 1);
-        assert_eq!(result[0].request.id, 2);
+        assert_eq!(result, false);
     }
 
     #[test]
-    fn test_filter_time() {
-        let mut request_response_pairs = get_fixture();
-        request_response_pairs[0].request.time = UTC::now().with_timezone(&request_response_pairs[0].request.time.timezone());
-        request_response_pairs[1].request.time = UTC::now().with_timezone(&request_response_pairs[1].request.time.timezone()) - Duration::minutes(12);
+    fn test_filter_time_matches() {
+        let mut pair = get_fixture();
+        pair.request.time = UTC::now().with_timezone(&pair.request.time.timezone());
 
         let conditions = FilterConditions {
             include_terms: None,
@@ -158,9 +179,24 @@ mod tests {
             latest_time: Some(Duration::minutes(10)),
         };
 
-        let result: Vec<RequestResponsePair> = filter(request_response_pairs, conditions);
+        let result = matches_filter(&pair, &conditions);
 
-        assert_eq!(result.len(), 1);
-        assert_eq!(result[0].request.id, 1);
+        assert_eq!(result, true);
+    }
+
+    #[test]
+    fn test_filter_time_matches_not() {
+        let mut pair = get_fixture();
+        pair.request.time = UTC::now().with_timezone(&pair.request.time.timezone()) - Duration::minutes(12);;
+
+        let conditions = FilterConditions {
+            include_terms: None,
+            exclude_terms: None,
+            latest_time: Some(Duration::minutes(10)),
+        };
+
+        let result = matches_filter(&pair, &conditions);
+
+        assert_eq!(result, false);
     }
 }
