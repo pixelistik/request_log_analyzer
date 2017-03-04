832ec84c9c155deab7b61d01790ce5e0607809ed
Pixelistik <code@pixelistik.de>
2017-03-04 20:13:41 +0100

    Extract HTTP handler into struct
    More decoupled than the capturing closure.

--- src/main.rs
+++ src/main.rs
@@ -66,24 +66,34 @@ fn main() {
 
 }
 
+struct HttpHandler {
+    args: args::RequestLogAnalyzerArgs,
+    run: fn(&args::RequestLogAnalyzerArgs) -> Option<analyzer::RequestLogAnalyzerResult>,
+}
+
+impl hyper::server::Handler for HttpHandler {
+    fn handle(&self, _: hyper::server::Request, mut res: hyper::server::Response) {
+        let result = (self.run)(&self.args);
+
+        let mut renderer = render::prometheus::PrometheusRenderer::new();
+        renderer.render(result.unwrap());
+        res.headers_mut()
+            .set(hyper::header::ContentType(renderer.encoder
+                .format_type()
+                .parse::<hyper::mime::Mime>()
+                .unwrap()));
+        res.send(&renderer.buffer).unwrap();
+    }
+}
+
 fn listen_http(args: args::RequestLogAnalyzerArgs, binding_address: &str) {
+    let handler = HttpHandler {
+        args: args,
+        run: run,
+    };
+
     info!("listening addr {:?}", binding_address);
-    hyper::server::Server::http(binding_address)
-        .unwrap()
-        .handle(move |_: hyper::server::Request, mut res: hyper::server::Response| {
-
-            let result = run(&args);
-
-            let mut renderer = render::prometheus::PrometheusRenderer::new();
-            renderer.render(result.unwrap());
-            res.headers_mut()
-                .set(hyper::header::ContentType(renderer.encoder
-                    .format_type()
-                    .parse::<hyper::mime::Mime>()
-                    .unwrap()));
-            res.send(&renderer.buffer).unwrap();
-        })
-        .unwrap();
+    hyper::server::Server::http(binding_address).unwrap().handle(handler).unwrap();
 }
 
 fn run(args: &args::RequestLogAnalyzerArgs) -> Option<analyzer::RequestLogAnalyzerResult> {
