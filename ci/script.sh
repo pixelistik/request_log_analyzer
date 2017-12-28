9a2df7705810e195d312da2e9a5628ec4bbd38c7
Pixelistik <code@pixelistik.de>
2017-12-28 23:26:49 +0100

    Less verbose CI runs
    There is no need to check both a normal build and a test suite build.
    There is no need to test both a debug and a release build.

--- ci/script.sh
+++ ci/script.sh
@@ -4,15 +4,15 @@ set -ex
 
 # TODO This is the "test phase", tweak it as you see fit
 main() {
-    cross build --target $TARGET
-    cross build --target $TARGET --release
+    #cross build --target $TARGET
+    #cross build --target $TARGET --release
 
     if [ ! -z $DISABLE_TESTS ]; then
         return
     fi
 
     cross test --target $TARGET
-    cross test --target $TARGET --release
+    #cross test --target $TARGET --release
 }
 
 # we don't run the "test phase" when doing deploys
